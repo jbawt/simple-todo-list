406951d4989e1143591c0880f12524543949dbb9
Josh Bawtinheimer <jbawtinheimer@gmail.com>
2020-11-19 12:31:46 -0700

    update about and add pictures

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="about">
     <h1>About</h1>
-    <p>This is the todo list app v1.0.0. It is part of the Vue crash course on youtube</p>
+    <p>This is the todo list app created as a result of a youtube following along for educational purposes. The main functionality is a result of the video but a lot of the styling is a result of my own creativity.</p>
   </div>
 </template>
+
+<style scoped>
+  p {
+    width: 80%;
+    margin-left: 10%;
+  }
+</style>
